5a9239f2f0df5e928a402c6948d8598e2f3deacd
Igor Znamensky <igor.znamensky@gmail.com>
2019-12-23 23:41:27 +0300

    update: fetch_site

--- aiohttp_asyncio.py
+++ aiohttp_asyncio.py
@@ -1,20 +1,43 @@
 import aiohttp
 import asyncio
+from bs4 import BeautifulSoup
 
 
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+# async def fetch(session, url):
+#     async with session.get(url) as response:
+#         return await response.text()
+#
+#
+# async def main(*args):
+#     if args:
+#         for url in args:
+#             async with aiohttp.ClientSession() as session:
+#                 html = await fetch(session, url)
+#                 print(html)
+
+async def fetch_site(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            html = await response.text()
+            soup = BeautifulSoup(html, 'html.parser')
+            print(url)
+            return html
+
+async def main():
+    task1 = asyncio.create_task(fetch_site('https://python.org'))
+    task2 = asyncio.create_task(fetch_site('https://ya.ru'))
+    task3 = asyncio.create_task(fetch_site('https://google.com'))
+
+    await asyncio.gather(
+        task1,
+        task2,
+        task3
+    )
 
 
-async def main(*args):
-    if args:
-        for url in args:
-            async with aiohttp.ClientSession() as session:
-                html = await fetch(session, url)
-                print(html)
 
 if __name__ == '__main__':
-    urls = ['http://python.org', 'https://aiohttp.readthedocs.io/en/stable/index.html']
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(*urls))
+    # urls = ['http://python.org', 'https://aiohttp.readthedocs.io/en/stable/index.html']
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main(*urls))
+    asyncio.run(main())
