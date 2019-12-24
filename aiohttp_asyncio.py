b2d92e587bb89101a0089a68f295d72f122fb26c
Igor Znamensky <igor.znamensky@gmail.com>
2019-12-24 22:09:38 +0300

    update: fetch_site

--- aiohttp_asyncio.py
+++ aiohttp_asyncio.py
@@ -1,35 +1,40 @@
+from time import time
 import aiohttp
 import asyncio
 from bs4 import BeautifulSoup
 import pprint
 
 
-async def fetch_site(url):
+async def fetch_content(url, session):
     print(f"load  {url}")
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            html = await response.text()
-            soup = BeautifulSoup(html, 'html.parser')
-            print(f"{url} has loaded")
-            return soup.find_all('meta')[:10]
+    async with session.get(url) as response:
+        html = await response.text()
+        soup = BeautifulSoup(html, 'html.parser')
+        print(f"{url} has loaded")
+        return soup.find_all('meta')[:10]
 
 
 async def main():
-    task1 = asyncio.create_task(fetch_site('https://python.org'))
-    task2 = asyncio.create_task(fetch_site('https://ya.ru'))
-    task3 = asyncio.create_task(fetch_site('https://google.com'))
+    urls = [
+        'https://ya.ru',
+        'https://ok.ru',
+        'https://python.org',
+    ]
 
-    a, b, c = await asyncio.gather(
-        task1,
-        task2,
-        task3
-    )
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        for i in range(3):
+            task = asyncio.create_task(fetch_content(urls[i], session))
+            tasks.append(task)
 
-    # print all lists, just to show
+        a, b, c = await asyncio.gather(*tasks)
+
+    # just to show
     for i in [a, b, c]:
         pprint.pprint(i)
-        print(len(i))
 
 
 if __name__ == '__main__':
+    t0 = time()
     asyncio.run(main())
+    print('Time to process requests:', time() - t0)
