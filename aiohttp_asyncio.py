e059b39306a88fee5cad7706b9721f00bf55b408
Igor Znamensky <igor.znamensky@gmail.com>
2019-12-24 12:19:45 +0300

    24/12/2019

--- aiohttp_asyncio.py
+++ aiohttp_asyncio.py
@@ -1,43 +1,35 @@
 import aiohttp
 import asyncio
 from bs4 import BeautifulSoup
+import pprint
 
-
-# async def fetch(session, url):
-#     async with session.get(url) as response:
-#         return await response.text()
-#
-#
-# async def main(*args):
-#     if args:
-#         for url in args:
-#             async with aiohttp.ClientSession() as session:
-#                 html = await fetch(session, url)
-#                 print(html)
 
 async def fetch_site(url):
+    print(f"load  {url}")
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             html = await response.text()
             soup = BeautifulSoup(html, 'html.parser')
-            print(url)
-            return html
+            print(f"{url} has loaded")
+            return soup.find_all('meta')[:10]
+
 
 async def main():
     task1 = asyncio.create_task(fetch_site('https://python.org'))
     task2 = asyncio.create_task(fetch_site('https://ya.ru'))
     task3 = asyncio.create_task(fetch_site('https://google.com'))
 
-    await asyncio.gather(
+    a, b, c = await asyncio.gather(
         task1,
         task2,
         task3
     )
 
+    # print all lists, just to show
+    for i in [a, b, c]:
+        pprint.pprint(i)
+        print(len(i))
 
 
 if __name__ == '__main__':
-    # urls = ['http://python.org', 'https://aiohttp.readthedocs.io/en/stable/index.html']
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main(*urls))
     asyncio.run(main())
